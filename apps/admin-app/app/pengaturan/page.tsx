e724af31de239bf9b939576a4f20802a185ab624
preettypeanuts <mrafly212001@gmail.com>
2025-04-14 13:35:10 +0700

    add tabs for settings section

--- apps/admin-app/app/pengaturan/page.tsx
+++ apps/admin-app/app/pengaturan/page.tsx
@@ -1,8 +1,23 @@
+import { Header } from "@shared/components/Header";
+import { Wrapper } from "@shared/components/Wrapper";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui-components/components/ui/tabs"; 
+
 export default async function PengaturanPage() {
-    return (
-      <div className='container'>
-      </div>
-    );
-  }
+  return (
+    <Wrapper className="min-h-full">
+      <Header label="Pengaturan" />
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList className=" bg-mainColor/20 dark:bg-mainColor/30">
+          <TabsTrigger value="account">Umum</TabsTrigger>
+          <TabsTrigger value="password">Profil Pengguna</TabsTrigger>
+          <TabsTrigger value="akses">Akses Pengguna</TabsTrigger>
+          <TabsTrigger value="notifikasi">Notifikasi</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </Wrapper>
+  );
+}
 
 
