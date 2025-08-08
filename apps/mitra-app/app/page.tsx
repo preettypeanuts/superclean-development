a8a5308cf46529a93a7afab7e5aef32dfb9be092
preettypeanuts <mrafly212001@gmail.com>
2025-08-08 17:26:21 +0700

    created riwayat pengerjaan

--- apps/mitra-app/app/page.tsx
+++ apps/mitra-app/app/page.tsx
@@ -1,11 +1,15 @@
 import { HomeBanner } from "libs/shared/src/components/mitra/home-banner"
 import { OngoingTask } from "libs/shared/src/components/mitra/ongoing-task"
+import { HistoryTask } from "libs/shared/src/components/mitra/history-task"
+import { TotalRating } from "libs/shared/src/components/mitra/total-rating"
 
 export default async function Index() {
   return (
-    <>
+    <main className="pb-[20vh] space-y-7">
       <HomeBanner />
       <OngoingTask />
-    </>
+      <HistoryTask />
+      <TotalRating />
+    </main>
   );
 }
