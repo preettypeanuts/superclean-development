67d174fa2e7135dea79fa9f1ba6ad8617d525563
preettypeanuts <mrafly212001@gmail.com>
2025-08-08 14:33:45 +0700

    createdd ongoing task

--- libs/shared/src/components/mitra/navigation-bar.tsx
+++ libs/shared/src/components/mitra/navigation-bar.tsx
@@ -1,24 +1,26 @@
 import { AiFillHome } from "react-icons/ai";
+import { RiNotification4Fill } from "react-icons/ri";
+import { MdAccountCircle } from "react-icons/md";
 
 export const NavigationBar = () => {
     return (
-        <nav className="fixed bottom-0 left-0 right-0 w-full h-[90px] bg-mainColor flex items-center justify-evenly">
-            <button className="flex flex-col items-center justify-center">
+        <nav className="fixed bottom-0 left-0 right-0 w-full h-[90px] shadow flex items-center justify-evenly">
+            <button className="flex flex-col gap-[4px] items-center justify-center text-muted-foreground">
                 <AiFillHome size={26} />
                 <p className="text-[14px]">
                     Home
                 </p>
             </button>
-              <button className="flex flex-col items-center justify-center">
-                <AiFillHome size={26} />
+              <button className="flex flex-col gap-[4px] items-center justify-center text-muted-foreground">
+                <RiNotification4Fill size={26} />
                 <p className="text-[14px]">
-                    Home
+                    Notifikasi
                 </p>
             </button>
-              <button className="flex flex-col items-center justify-center">
-                <AiFillHome size={26} />
+              <button className="flex flex-col gap-[4px] items-center justify-center text-muted-foreground">
+                <MdAccountCircle size={26} />
                 <p className="text-[14px]">
-                    Home
+                    Akun
                 </p>
             </button>
         </nav>
