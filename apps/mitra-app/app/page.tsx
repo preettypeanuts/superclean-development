97521e57f7d549ca9162c2b69b6575dd6e546e2a
preettypeanuts <mrafly212001@gmail.com>
2025-08-08 15:30:41 +0700

    created ongoing task

--- apps/mitra-app/app/page.tsx
+++ apps/mitra-app/app/page.tsx
@@ -1,9 +1,11 @@
 import { HomeBanner } from "libs/shared/src/components/mitra/home-banner"
+import { OngoingTask } from "libs/shared/src/components/mitra/ongoing-task"
 
 export default async function Index() {
   return (
     <>
       <HomeBanner />
+      <OngoingTask />
     </>
   );
 }
