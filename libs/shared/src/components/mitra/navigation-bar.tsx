765ee6e58759c4294111dc7a3e2a99fa7a58057b
preettypeanuts <mrafly212001@gmail.com>
2025-08-12 10:52:17 +0700

    created total rating widget

--- libs/shared/src/components/mitra/navigation-bar.tsx
+++ libs/shared/src/components/mitra/navigation-bar.tsx
@@ -4,11 +4,11 @@ import { MdAccountCircle } from "react-icons/md";
 
 export const NavigationBar = () => {
     return (
-        <nav className="fixed bottom-0 left-0 right-0 w-full h-[90px] shadow flex items-center justify-evenly backdrop-blur-xl bg-white/70 rounded-t-lg">
-            <button className="flex flex-col gap-[4px] items-center justify-center text-muted-foreground">
+        <nav className="fixed bottom-0 left-0 right-0 w-full h-[90px] shadow-secondaryShadow flex items-center justify-evenly backdrop-blur-xl bg-white/70 rounded-t-2xl">
+            <button className="flex flex-col gap-[4px] items-center justify-center text-mainColor">
                 <AiFillHome size={26} />
-                <p className="text-[14px]">
-                    Home
+                <p className="text-[14px] text-mainColor">
+                    Beranda
                 </p>
             </button>
               <button className="flex flex-col gap-[4px] items-center justify-center text-muted-foreground">
