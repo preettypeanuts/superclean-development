4c590a21d652538a382df2ddbf76e4bd53f5fc7d
preettypeanuts <mrafly212001@gmail.com>
2025-08-12 11:30:13 +0700

    created page banner

---
+++ apps/mitra-app/app/daftar-spk/page.tsx
@@ -0,0 +1,10 @@
+import { PageBanner } from "libs/shared/src/components/mitra/page-banner"
+
+
+export default function DaftarSPK() {
+    return (
+        <main className="pb-[20vh] space-y-7">
+            <PageBanner />
+        </main>
+    )
+}
---
+++ libs/shared/src/components/mitra/page-banner.tsx
@@ -0,0 +1,22 @@
+import { BsArrowLeft } from "react-icons/bs";
+
+export const PageBanner = () => {
+    return (
+        <main className="min-h-[325px]">
+            <section className="w-full h-[147px] bg-gradient-to-r from-mainColor from-10% to-mainDark to-110% rounded-b-2xl flex items-center relative">
+                <div className="mx-5 w-full grid grid-cols-3">
+                    <div>
+                        <button className="w-[30px] h-[30px] bg-white/80 rounded-lg flex items-center justify-center">
+                            <BsArrowLeft size={20} color="#30635D" />
+                        </button>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <p className="text-[20px] font-medium text-white text-center">
+                            Daftar SPK
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
