704679a30648d8af4977a07f9b4c1c1e8523b366
preettypeanuts <mrafly212001@gmail.com>
2025-08-12 17:01:44 +0700

    reusable page banner

--- apps/mitra-app/app/rating/page.tsx
+++ apps/mitra-app/app/rating/page.tsx
@@ -1,13 +1,14 @@
 import { PageBanner } from "libs/shared/src/components/mitra/page-banner"
 import { DaftarSPKTabs } from "libs/shared/src/components/mitra/daftar-spk-tabs"
 
-export default function DaftarSPK() {
+export default function RatingPage() {
     return (
         <main className="pb-[20vh] space-y-7">
             <PageBanner
-                title="Detail Produk"
+                title="Profile"
+                variant="white"
+                size="compact"
             />
-            <DaftarSPKTabs />
         </main>
     )
 }
